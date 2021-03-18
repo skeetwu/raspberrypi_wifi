6298ef2ff394b32c8875e8ebe5a23f4d6f81c261
Serena LI <serena_li279@163.com>
2021-03-18 10:25:01 +0000

    change function return type

--- start_wifi.sh
+++ start_wifi.sh
@@ -14,7 +14,6 @@ check_wifi_info()
     echo "无WIFI信息" >>$LOG_DIR
     wifi_info_existing=1
   fi
-
   return $wifi_info_existing
 }
 
@@ -39,7 +38,6 @@ check_wifi_status()
     fi
     sleep 30s
   done
-  echo $tmp_wifi_enabled_flag
   return $tmp_wifi_enabled_flag
 }
 
@@ -65,7 +63,6 @@ check_AP_status()
     fi
     sleep 5s
   done
-  echo $tmp_AP_status_flag
   return $tmp_AP_status_flag
 }
 
@@ -88,7 +85,8 @@ echo `date "+%y-%m-%d %H:%M:%S"` >>$LOG_DIR 2>&1
 
 while true
 do
-  wifi_info_flag=check_wifi_info
+  check_wifi_info
+  wifi_info_flag=$?
   # check 是否有WIFI信息
   if $wifi_info_flag; then
     # 有WIFI信息
@@ -99,7 +97,8 @@ do
       echo "有WIFI信息,WIFI连接失败，即将重启WIFI..." >>$LOG_DIR
       wpa_cli -i wlan0 reconfigure >>$LOG_DIR 2>&1
       # 因为是重启WIFI 多check几次
-      tmp_wifi_enabled_flag=`check_wifi_status`
+      check_wifi_status
+      tmp_wifi_enabled_flag=$?
       if [ "$tmp_wifi_enabled_flag" = "1" ]; then
         echo "WIFI连接失败,WIFI信息有误，删除错误信息并重启热点..." >>$LOG_DIR
         delete_wifi_info
@@ -108,7 +107,8 @@ do
     fi
   else
     # 无WIFI信息
-    AP_enabled_flag=`check_AP_status`
+    check_AP_status
+    AP_enabled_flag=$?
     echo $AP_enabled_flag
     if [ "$AP_enabled_flag" = "1" ]; then
       echo "无WIFI信息，热点未启动，即将启动热点..." >>$LOG_DIR
